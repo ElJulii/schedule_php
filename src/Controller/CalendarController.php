49548e2674b1966adbcf1cbb0e62bc0621541647
Julian <juli2003juli@gmail.com>
2024-11-10 15:50:55 +0300

    first part

---
+++ src/Controller/CalendarController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CalendarController extends AbstractController
+{
+    // ? значит что, month может быть NULL
+    #[Route('/calendar/{viewType}/{month?}', name: 'app_calendar', defaults: ['viewType' => 'list', 'month' => null])]
+    public function index(Request $request, string $viewType, ?int $month): Response
+    {
+        $currentDate = new \DateTime();
+
+        if ($month) {
+            $currentDate->setDate($currentDate->format('Y'), $month, 1);
+        }
+
+        $firstDayOfMonth = (clone $currentDate)->modify('first day of this month'); //Среда
+        $firstDayOfWeek = (int) $firstDayOfMonth->format('N'); // 3 - среда
+
+        //Количество дней в месяце
+        $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $currentDate->format('m'), $currentDate->format('Y'));
+        $calendarDays = [];
+
+        // Добавлять свободные места до первого дня месяца
+        for ($i = 1; $i < $firstDayOfWeek; $i++) {
+            $calendarDays[] = null;
+        }
+
+        // Добавлять остальные дни
+        for ($day = 1; $day <= $daysInMonth; $day++) {
+            $date = (clone $currentDate)->setDate($currentDate->format('Y'), $currentDate->format('m'), $day);
+            $calendarDays[] = [
+                'date' => $date,
+                'isWeekend' => in_array($date->format('N'), [6, 7]), // Sábado y domingo
+            ];
+        }
+
+        $weeks = array_chunk($calendarDays, 7);
+
+        return $this->render("calendar/" . $viewType . ".html.twig", [
+            'weeks' => $weeks,
+            'currentDate' => $currentDate
+        ]);
+    }
+
+}
